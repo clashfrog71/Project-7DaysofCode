e6ecea3d8360a381c615e53da3a9d7edcc57e060
Tiago Pereira <clashfrog71@gmail.com>
2025-04-28 00:10:24 -0300

    fazer uma requisição para a api

--- Project 7DaysofCode/Program.cs
+++ Project 7DaysofCode/Program.cs
@@ -1,7 +1,25 @@
-﻿public class program
+﻿using System.Text.Json;
+
+using (HttpClient client = new HttpClient())
 {
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("Menu API do Pokemon");
-    }
+
+
+  
+
+        try
+        {
+            string resposta = await client.GetStringAsync("https://pokeapi.co/api/v2/ability/65/");
+        
+            var musicas = JsonSerializer.Deserialize<List<pokemon>>(resposta)!;
+
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine($"Erro na requisição: {e.Message}");
+        }
+    
+}public class pokemon
+{
+    public string name { get; set; }
 }
+
