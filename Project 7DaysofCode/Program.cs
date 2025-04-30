bbc97fb4c5127c1c9fd79173bf12b75a91c66258
Tiago Pereira <clashfrog71@gmail.com>
2025-04-29 21:46:55 -0300

    desserializar json e selecionar coisas especificas

--- Project 7DaysofCode/Program.cs
+++ Project 7DaysofCode/Program.cs
@@ -1,12 +1,21 @@
 ﻿using RestSharp;
 using System.Text.Json;
+using System.Xml.Linq;
 
 public class program
 {
-    public static void Main()
+    async static Task Main()
     {
-        var client = new RestClient(https://pokeapi.co/api/v2/pokemon/);
-        var request = new RestRequest(Method.GET);
+        var client = new RestClient("https://pokeapi.co/api/v2/pokemon/");
+        var request = new RestRequest("", Method.Get);
+        var response = await client.ExecuteAsync(request);
+        var b = JsonSerializer.Deserialize<TestObject>(response.Content);
+        Console.WriteLine(b.count);
     }
+}
+class TestObject
+{
+    public int count { get; set; }
+
 }
 
