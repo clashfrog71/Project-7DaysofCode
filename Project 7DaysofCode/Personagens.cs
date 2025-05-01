806962619f362cf94b3fcd8f0138530c1d57f50f
Tiago Pereira <clashfrog71@gmail.com>
2025-05-01 10:37:12 -0300

    armazenar informações de adoção do pokemon

---
+++ Project 7DaysofCode/Personagens.cs
@@ -0,0 +1,58 @@
+﻿using RestSharp;
+using System.Text.Json;
+
+namespace Project_7DaysofCode
+{
+    public class pokemon
+    {
+        public pokemon(string name, string url)
+        {
+            this.name = name;
+            this.url = url;
+        }
+
+        public string name { get; set; }
+        public string url { get; set; }
+        public void ExibirPokemon()
+        {
+            Console.WriteLine($"Nome: {this.name}");
+            Console.WriteLine($"Url: {this.url}");
+        }
+
+        public void ExibirPokemonInfoTotal()
+        {
+            var client = new RestClient($"{this.url}");
+            var request = new RestRequest("", Method.Get);
+            var response = client.Execute(request);
+            PokemonInfoTotal Json = JsonSerializer.Deserialize<PokemonInfoTotal>(response.Content);
+
+            Console.WriteLine($"Nome: {this.name}");
+            Console.WriteLine($"Altura: {Json.height * 10}cm");
+            Console.WriteLine($"Peso: {Json.weight}kg");
+        }
+
+        internal class PokemonInfoTotal
+        {
+            public int weight { get; set; }
+            public int height { get; set; }
+            internal int base_experience { get; set; }
+
+        }
+    }
+
+    public class Jogador
+    {
+        public Jogador(string nome, pokemon pokemon)
+        {
+            this.Nome = nome;
+            this.Pokemon = pokemon;
+        }
+        public string Nome { get; set; }
+        public pokemon Pokemon { get; set; }
+        public void ExibirJogador()
+        {
+            Console.WriteLine($"Nome: {this.Nome}");
+            Console.WriteLine($"Pokemon: {this.Pokemon.name}");
+        }
+    }
+}
--- Project 7DaysofCode/Program.cs
+++ Project 7DaysofCode/Program.cs
@@ -1,4 +1,5 @@
-﻿using RestSharp;
+﻿using Project_7DaysofCode;
+using RestSharp;
 using System.Text.Json;
 using System.Xml.Linq;
 
@@ -6,12 +7,7 @@ public class program
 {
     async static Task Main()
     {
-        var client = new RestClient("https://pokeapi.co/api/v2/pokemon/");
-        var request = new RestRequest("", Method.Get);
-        var response = await client.ExecuteAsync(request);
-        TestObject Json = JsonSerializer.Deserialize<TestObject>(response.Content);
-        Console.WriteLine("Bem vindo ao desafio de 7 dias!");
-        Console.WriteLine("1: Adotar pokemon");
+        Menu.ExibirMenu();
         var resposta = Console.ReadLine();
         if (int.TryParse(resposta, out int numero))
         {
@@ -19,6 +15,13 @@ public class program
             {
                 case 1:
                     await funcoes.AdotarPokemon();
+                    break;
+                case 0:
+                    Console.Clear();
+                    Console.WriteLine("tchau");
+                    break;
+                default:
+                    Console.WriteLine("Opção Invalida");
                     break;
             }
         }
@@ -28,44 +31,15 @@ public class program
         }
     }
     }
-
-public class TestObject
+public class TotalPokemons
 {
     public int count { get; set; }
     public List<pokemon> results { get; set; }
-    public void contador()
+    public void Contador()
     {
         Console.WriteLine($"Total de pokemons: {this.count}");
     }
 
 }
-public class pokemon
-{
-    public string name { get; set; }
-    public string url { get; set; }
-    public void ExibirPokemon()
-    {
-        Console.WriteLine($"Nome: {this.name}");
-        Console.WriteLine($"Url: {this.url}");
-    }
-}
-public static class funcoes
-{
-    public static async Task AdotarPokemon()
-    {
-        var client = new RestClient("https://pokeapi.co/api/v2/pokemon/");
-        var request = new RestRequest("", Method.Get);
-        var response = await client.ExecuteAsync(request);
-        TestObject Json = JsonSerializer.Deserialize<TestObject>(response.Content);
-        Console.Clear();
-        Console.WriteLine("Adote um pokemon!");
-        foreach (var item in Json.results)
-        {
-            Console.WriteLine(item.name); 
-        }
-        var resposta = Console.ReadLine().ToLower();
-        var oescolhido = Json.results.FirstOrDefault(p => p.name.ToLower() == resposta);
-        Console.WriteLine($"você vai adotar o {oescolhido.name}");
-    }
-}
+
 
